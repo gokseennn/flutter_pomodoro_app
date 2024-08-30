75a56518a1ce916f8d70ac4ca51445a5ff20c236
gokseennn <ahmet.goksen81@icloud.com>
2024-08-30 16:49:52 +0300

    env bug fix

--- lib/common/environment/environment_configs/development_config.dart
+++ lib/common/environment/environment_configs/development_config.dart
@@ -2,7 +2,7 @@ import '../i_environment_config.dart';
 
 class DevelopmentConfig extends IEnvrionmentConfig {
   @override
-  String get apiHost => "localhost:8080";
+  String get apiHost => "http://localhost:8080";
   @override
-  String get host => "localhost:8080";
+  String get host => "http://localhost:8080";
 }
